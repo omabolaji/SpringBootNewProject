9b4b69d4c20459879313d2de905f507897dd08e7
Bolaji Oyewumi <49565233+omabolaji@users.noreply.github.com>
2019-12-20 12:23:01 +0100

    Add files via upload

---
+++ SpringBootRestFull/src/main/java/com/famousb/webservices/SpringBootRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.famousb.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestFullApplication.class, args);
+	}
+
+}
---
+++ SpringBootRestFull/src/test/java/com/famousb/webservices/SpringBootRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.famousb.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
